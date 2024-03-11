ebe79dfda2e314ff852e9b833a42f3c73348d07e
krateng <git.noreply@krateng.ch>
2024-03-11 05:21:27 +0100

    Rework entity definition logic

--- tirganach/entities.py
+++ tirganach/entities.py
@@ -3,50 +3,62 @@ class Field:
 	len_bytes: int
 	data_type: type
 
-	def __init__(self, offset: int, len_bytes: int, data_type: type):
+	def __init__(self, offset: int, len_bytes: int, data_type: type = None):
 		self.offset = offset
 		self.len_bytes = len_bytes
 		self.data_type = data_type
 
 
 class Entity:
-	fields: dict[str, Field]
+	_fields: dict[str, Field]
 
 	def __init__(self, raw_bytes):
 		#print("IN:", ' '.join(format(byte, '02x') for byte in raw_bytes))
-		for fname, finfo in self.fields.items():
-			byte_source = raw_bytes[finfo.offset:finfo.offset+finfo.len_bytes]
-			if finfo.data_type is int:
+		for field_name, field_info in self._fields.items():
+			byte_source = raw_bytes[field_info.offset:field_info.offset+field_info.len_bytes]
+			if field_info.data_type is int:
 				val = int.from_bytes(byte_source, byteorder='little', signed=True)
-			elif finfo.data_type is str:
+			elif field_info.data_type is str:
 				val = byte_source.rstrip(b'\x00').decode('utf-8')
+			elif field_info.data_type is bool:
+				assert len(byte_source) == 1
+				val = (byte_source[0] != 0)
 			else:
 				raise NotImplemented()
-			self.__setattr__(fname, val)
+			self.__setattr__(field_name, val)
 
 	def _to_bytes(self):
 		result = bytearray(b'\x00' * self.length())
-		for fname, finfo in self.fields.items():
-			source = self.__getattribute__(fname)
-			if finfo.data_type is int:
-				val = source.to_bytes(finfo.len_bytes, byteorder='little', signed=True)
-			elif finfo.data_type is str:
-				val = source.encode('utf-8').ljust(finfo.len_bytes, b'\x00')
+		for field_name, field_info in self._fields.items():
+			source = self.__getattribute__(field_name)
+			if field_info.data_type is int:
+				val = source.to_bytes(field_info.len_bytes, byteorder='little', signed=True)
+			elif field_info.data_type is str:
+				val = source.encode('utf-8').ljust(field_info.len_bytes, b'\x00')
+			elif field_info.data_type is bool:
+				val = b'\x01' if source else b'\x00'
 			else:
 				raise NotImplemented()
-			result[finfo.offset:finfo.offset+finfo.len_bytes] = val
+			result[field_info.offset:field_info.offset+field_info.len_bytes] = val
 		#print("OUT:", ' '.join(format(byte, '02x') for byte in result))
 		return bytes(result)
 
 	@classmethod
 	def length(cls):
-		return max(f.offset+f.len_bytes for f in cls.fields.values())
+		return max(f.offset+f.len_bytes for f in cls._fields.values())
 
 	def __init_subclass__(cls):
-		# just make sure our definitions dont overlap
+		# save info in _fields
+		cls._fields = {}
+		for field_name, field_type in cls.__annotations__.items():
+			cls._fields[field_name] = getattr(cls, field_name)
+			cls._fields[field_name].data_type = field_type
+			field_info = cls._fields[field_name]
+
+		# make sure our definitions dont overlap
 		bytes_accounted = set()
-		for finfo in cls.fields.values():
-			for b in range(finfo.offset, finfo.len_bytes):
+		for field_info in cls._fields.values():
+			for b in range(field_info.offset, field_info.offset + field_info.len_bytes):
 				assert b not in bytes_accounted
 				bytes_accounted.add(b)
 
@@ -55,35 +67,30 @@ class Entity:
 
 class Item(Entity):
 
-	fields = {
-		'item_id': Field(offset=0, len_bytes=2, data_type=int),
-		'strength': Field(offset=2, len_bytes=2, data_type=int),
-		'stamina': Field(offset=4, len_bytes=2, data_type=int),
-		'agility': Field(offset=6, len_bytes=2, data_type=int),
-		'dexterity': Field(offset=8, len_bytes=2, data_type=int),
-		'health': Field(offset=10, len_bytes=2, data_type=int),
-		'charisma': Field(offset=12, len_bytes=2, data_type=int),
-		'intelligence': Field(offset=14, len_bytes=2, data_type=int),
-		'wisdom': Field(offset=16, len_bytes=2, data_type=int),
-		'mana': Field(offset=18, len_bytes=2, data_type=int),
-		'armor': Field(offset=20, len_bytes=2, data_type=int),
-		'resist_fire': Field(offset=22, len_bytes=2, data_type=int),
-		'resist_ice': Field(offset=24, len_bytes=2, data_type=int),
-		'resist_black': Field(offset=26, len_bytes=2, data_type=int),
-		'resist_mental': Field(offset=28, len_bytes=2, data_type=int),
-		'speed_run': Field(offset=30, len_bytes=2, data_type=int),
-		'speed_fight': Field(offset=32, len_bytes=2, data_type=int),
-		'speed_cast': Field(offset=34, len_bytes=2, data_type=int)
-	}
+	item_id: int = Field(0, 2)
+	strength: int = Field(2, 2)
+	stamina: int = Field(4, 2)
+	agility: int = Field(6, 2)
+	dexterity: int = Field(8, 2)
+	health: int = Field(10, 2)
+	charisma: int = Field(12, 2)
+	intelligence: int = Field(14, 2)
+	wisdom: int = Field(16, 2)
+	mana: int = Field(18, 2)
+	armor: int = Field(20, 2)
+	resist_fire: int = Field(22, 2)
+	resist_ice: int = Field(24, 2)
+	resist_black: int = Field(26, 2)
+	resist_mind: int = Field(28, 2)
+	speed_run: int = Field(30, 2)
+	speed_fight: int = Field(32, 2)
+	speed_cast: int = Field(34, 2)
 
 
 class Localisation(Entity):
 
-	fields = {
-		'text_id': Field(offset=0, len_bytes=2, data_type=int),
-		'language_id': Field(offset=2, len_bytes=1, data_type=int),
-		'is_dialogue': Field(offset=3, len_bytes=1, data_type=int),
-		'dialogue_name': Field(offset=4, len_bytes=50, data_type=str),
-		'text': Field(offset=54, len_bytes=512, data_type=str)
-	}
-
+	text_id: int = Field(0, 2)
+	language_id: int = Field(2, 1)
+	is_dialogue: bool = Field(3, 1)
+	dialogue_name: str = Field(4, 50)
+	text: str = Field(54, 512)
