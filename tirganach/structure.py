035399ae833bf2f5b70b009ab5f52cee321d2e81
krateng <git.noreply@krateng.ch>
2024-03-12 16:25:28 +0100

    Expand structure definition

--- tirganach/structure.py
+++ tirganach/structure.py
@@ -1,7 +1,8 @@
 from os import PathLike
 from typing import Type, IO, get_origin, get_args
 
-from tirganach.entities import Armor, Localisation, Entity, ItemRequirement, Building
+from tirganach.entities import Armor, Localisation, Entity, ItemRequirement, Building, BuildingRequirement, Creature, \
+	CreatureStats
 
 
 class Table(list):
@@ -20,6 +21,7 @@ class Table(list):
 class GameData:
 	fields: dict[str, tuple[Type[Entity], int]] = {}
 	raw: bytearray
+	_length: int
 
 	def __init__(self, from_input: bytes | str | PathLike[bytes]):
 		if isinstance(from_input, PathLike) or isinstance(from_input, str):
@@ -29,6 +31,7 @@ class GameData:
 			raw = from_input
 
 		self.raw = bytearray(raw)
+		assert len(self.raw) == self._length
 
 		for fname, finfo in self.fields.items():
 			entity_type, offset, *other = finfo
@@ -69,11 +72,30 @@ class GameData:
 
 
 class GameData154(GameData):
+	_length = 66859922
+	_md5 = [
+		'4a5025f4cc40aab46efde848e2a4682d',
+		'7c1d33607b8920440b28c75d0db9c4ae', # polish
+		'c09948dc3e9e2908383f844ec2b4a976' # russian
+	]
+
 	fields = {
-		# just testing so far
-		'items': (Table[Armor], 0x8a71d, 462), # 0, 461
-		'localisation': (Table[Localisation], 0x12f2e3, 115065), # 20, 43311, 95404
-		'item_requirements': (Table[ItemRequirement], 0x99e21, 4), # 0
-		'buildings': (Table[Building], 0x3f81c94, 140)
+		'items': (Table[Armor], 0x897e8, 635),
+		'localisation': (Table[Localisation], 0x12d177, 115080),
+		'item_requirements': (Table[ItemRequirement], 0x94450, 5104),
+		'buildings': (Table[Building], 0x3f81c69, 207),
+		'building_requirements': (Table[BuildingRequirement], 0x3f85bbe, 166),
+		'creatures': (Table[Creature], 0x3f4cde6, 2617),
+		'creature_stats': (Table[CreatureStats], 0x4421D, 2536)
 	}
 
+
+class GameData161(GameData):
+	_length = 66859988
+
+	fields = {
+		'items': (Table[Armor], 0x8a71d, 500+0), # 0, 461
+		'localisation': (Table[Localisation], 0x12f2e3, 115065), # 20, 43311, 95404
+		'item_requirements': (Table[ItemRequirement], 0x99e21, 100+0), # 0
+		'buildings': (Table[Building], 0x3f81c94, 140)
+	}
